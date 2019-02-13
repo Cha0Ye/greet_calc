7490a2a8ec4a3e4acd1e4ab5233aa26eaedd9fb3
Chao Ye <Chao12.ye@gmail.com>
2019-02-13 13:28:28 -0800

    completed calculator

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_operation():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))    
+    result = add(a,b)
+    return f'{result}'
+
+@app.route('/sub')
+def sub_operation():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))    
+    result = sub(a,b)
+    return f'{result}'
+
+@app.route('/mult')
+def mult_operation():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))    
+    result = mult(a,b)
+    return f'{result}'
+
+@app.route('/div')
+def div_operation():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))    
+    result = div(a,b)
+    return f'{result}'
+
+
